bfadff4d217ee62344810ff054ba7c4ac94d6544
sohaib <souhaibemanah@gmail.com>
2023-12-22 00:37:43 +0100

    + add sign in with jwt

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use serde::Serialize;
 
 pub fn validate_user_role(role: &str) -> bool {
     match role {
@@ -21,4 +22,11 @@ pub fn sanitize_input(input: String) -> String {
     let re = Regex::new(r"[^a-zA-Z0-9]").unwrap();
     let sanitized_input = re.replace_all(input.to_owned().as_str(), "").to_string();
     sanitized_input
+}
+
+// Define the struct for the claims
+#[derive(Debug, Serialize)]
+pub(crate) struct Claims {
+    pub(crate) username: String,
+    pub(crate) role: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+use std::env;
 use bcrypt::{DEFAULT_COST, hash};
+use dotenv::dotenv;
+use jsonwebtoken::{Algorithm, encode, EncodingKey, Header};
 use redis::{Commands, RedisResult};
 use tonic::{Request, Response, Status, transport::Server};
 use authentication::{authentication_server::{Authentication, AuthenticationServer}, SignInRequest, SignInResponse, SignUpRequest, SignUpResponse};
 use database::redis_connection::redis_connect;
+
 
 mod database;
 mod models;
@@ -111,8 +115,79 @@ impl Authentication for AuthenticationService {
     }
 
     async fn sign_in(&self, request: Request<SignInRequest>) -> Result<Response<SignInResponse>, Status> {
-        todo!()
+        // get the user from the request
+        let user = request.into_inner();
+
+        // get the username and password from the request
+        let username = helpers::sanitize_input(user.username);
+        let password = helpers::sanitize_input(user.password);
+
+        // connect to redis
+        let mut redis_connection = redis_connect();
+
+        // check if the user exists in redis
+        let redis_key = format!("user:{}", username);
+        let redis_value: RedisResult<String> = redis_connection.get(&redis_key);
+        let role;
+
+        // if the user doesn't exist in redis, check in the database
+        if redis_value.is_err() {
+            // connect to mysql
+            let mut mysql_connection = database::mysql_connection::mysql_connect();
+
+            // check if the user exists in the database
+            let result = models::user::User::find_user_by_username(&mut mysql_connection, &username);
+            if result.is_err() {
+                return Err(Status::not_found("User not found"));
+            }
+
+            // get the user from the database
+            let db_user = result.unwrap().unwrap();
+
+            // get the role from the database
+            role = db_user.role;
+
+            // compare the hashed password from the database with the hashed version of the password from the request
+            if bcrypt::verify(&password, &db_user.password_hash).is_err() {
+                return Err(Status::unauthenticated("Invalid password"));
+            }
+        } else {
+            // get the user from redis
+            let redis_user = redis_value.unwrap();
+
+            // the value in redis is in the format "hashed_password:role", so split it to get the hashed password
+            let redis_hashed_password = redis_user.split(":").next().unwrap();
+            role = redis_user.split(":").last().unwrap().to_string();
+
+            // compare the hashed password from redis with the hashed version of the password from the request
+            if bcrypt::verify(&password, redis_hashed_password).is_err() {
+                return Err(Status::unauthenticated("Invalid password"));
+            }
+        }
+        // Define the secret key
+        dotenv().ok();
+        let secret_key = env::var("JWT_SECRET_KEY").expect("JWT_SECRET_KEY must be set");
+
+        // Create the claims
+        let claims = helpers::Claims {
+            username,
+            role,
+        };
+
+        let token = encode(&Header::new(Algorithm::HS256), &claims, &EncodingKey::from_secret(secret_key.as_ref()))
+            .expect("Failed to encode claims into JWT");
+
+
+        // return a success response
+        let response = SignInResponse {
+            success: true,
+            message: "User signed in successfully".into(),
+            jwt: token,
+        };
+        return Ok(Response::new(response));
     }
 }
 
 // todo imp singleton pattern in connection
+//  todo right test
+
